57e455ab2204aa4451f8da48230d18812d1cd1b4
MoAlhaddad <mohamadalhaddad25@gmail.com>
2024-06-07 20:14:49 -0400

    reached 1hr mark

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {FiDownload} from "react-icons/fi";
 
-export default function Home() {
+//components
+import Social from "@/components/Social";
+
+const Home = () => {
   return (
-    <main  >
-      Homepage
-    </main>
-  );
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+     {/* text */}
+     <div className="text-center xl:text-left">
+      <span className="text-xl">Software Developer</span>
+      <h1 className="h1">Hello I'm <br /> <span className="text-accent">Mohamad Alhaddad</span></h1>
+     <p className="max-1-[500px] mb-9 text-white/80">I specialize in developing sophisticated digital solutions and possess expertise in a wide range of programming languages and technologies</p>
+     {/* btn and socials*/}
+     <div className="flex flex-col xl:flex-row items-center gap-8">
+      <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+        <span>Download CV</span>
+        <FiDownload className="text-xl" />
+      </Button>
+      <div className="mb-8 xl:mb-0">
+        <Social/>
+      </div>
+     </div>
+     
+     </div>
+      {/** photo */}
+      <div>photo</div>
+     </div>
+      </div>
+    </section>
+  )
 }
+
+export default Home
---
+++ components/Social.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import {FaGithub, FaLinkedIn} from "react-icons/fa";
+
+const socials = [
+    {icon: <FaGithub />, path: ""},
+    {icon: <FaLinkedIn/>, path: ""}
+]
+
+const Social = ({containerStyles, iconStyles}) => {
+  return (
+    <div>Social</div>
+  )
+}
+
+export default Social
