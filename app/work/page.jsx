440e32348f37264b1ac574440a66ce43b932a290
MoAlhaddad <mohamadalhaddad25@gmail.com>
2024-06-11 21:33:54 -0400

    2hr15min

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,4 +1,24 @@
-import React from 'react'
+"use client";
+import {motion} from "framer-motion";
+import React, {useState} from "react";
+import {Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import {BsArrowUpRight, BsGithub} from "react-icons/bs";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: '01',
+    category: 'frontend',
+    title: 'Youtube Clone',
+    description: "youtube clone"
+  }
+]
+
+
+
 
 const Work = () => {
   return (
